bdb44fde8b3cdc4c914cf03013efd19c65bbbf86
Your Name <rhea72345@gmail.com>
2026-07-19 16:07:54 +0530

    redeploy 6

--- Server/inngest/index.js
+++ Server/inngest/index.js
@@ -1,26 +1,34 @@
-// 
+import { Inngest } from "inngest";
+import User from "../models/User.js";
 
+// Create a client to send and receive events
+export const inngest = new Inngest({ id: "movie-ticket-booking" });
+
+// inngest function to save user data to a database
 const syncUserCreation = inngest.createFunction(
   {
     id: "sync-user-from-clerk",
-    triggers: { event: "clerk/user.created" }, // ✅
+    trigger: { event: "clerk/user.created" },
   },
   async ({ event }) => {
     const { id, first_name, last_name, email_addresses, image_url } = event.data;
+
     const userData = {
       _id: id,
       email: email_addresses[0].email_address,
       name: first_name + " " + last_name,
       image: image_url,
     };
+
     await User.create(userData);
   }
 );
 
+// inngest function to delete user data from database
 const syncUserDeletion = inngest.createFunction(
   {
     id: "delete-user-with-clerk",
-    triggers: { event: "clerk/user.deleted" }, // ✅
+    trigger: { event: "clerk/user.deleted" },
   },
   async ({ event }) => {
     const { id } = event.data;
@@ -28,19 +36,28 @@ const syncUserDeletion = inngest.createFunction(
   }
 );
 
+// inngest function to update user data in database
 const syncUserUpdation = inngest.createFunction(
   {
     id: "update-user-from-clerk",
-    triggers: { event: "clerk/user.updated" }, // ✅
+    trigger: { event: "clerk/user.updated" },
   },
   async ({ event }) => {
     const { id, first_name, last_name, email_addresses, image_url } = event.data;
+
     const userData = {
       _id: id,
       email: email_addresses[0].email_address,
       name: first_name + " " + last_name,
       image: image_url,
     };
+
     await User.findByIdAndUpdate(id, userData);
   }
 );
+
+export const functions = [
+  syncUserCreation,
+  syncUserDeletion,
+  syncUserUpdation,
+];
