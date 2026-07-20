5b1bfd5fb86b6c17068987d521180d6072dc9f7d
sonakshi mandal <sonakshimandal21@gmail.com>
2026-07-20 10:07:14 +0530

    Update after pull

---
+++ server/server.js
@@ -0,0 +1,49 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import connectDB from "./configs/db.js";
+import dns from "dns";
+import { clerkMiddleware } from "@clerk/express";
+import { serve } from "inngest/express";
+import { inngest, functions } from "./inngest/index.js";
+
+// Change DNS
+dns.setServers(["1.1.1.1", "8.8.8.8"]);
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Connect Database
+await connectDB();
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+app.use(clerkMiddleware());
+
+// Routes
+app.get("/", (req, res) => {
+   res.send("Server is Live!");
+ });
+
+app.get("/test", (req, res) => {
+  res.json({ success: true });
+});
+
+app.use(
+  "/api/inngest",
+  serve({
+    client: inngest,
+    functions,
+  })
+);
+
+// Start server only in local development
+if (process.env.NODE_ENV !== "production") {
+  app.listen(port, () => {
+    console.log(`Server listening at http://localhost:${port}`);
+  });
+}
+
+// Export app for Vercel
+export default app;
